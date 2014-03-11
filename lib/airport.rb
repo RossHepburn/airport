f94b937cd70b60002e8982bf7d89f432f92dcf6f
Ross Hepburn <hepburnjrl@gmail.com>
2014-03-11 14:59:57 +0000

    PASSED - with a couple of edits

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,27 +3,27 @@ class Airport
 	attr_accessor :capacity
 	attr_reader :planes	
 
-	def initialize(capacity = 15)
+	def initialize(weather, capacity = 15)
 		@capacity = capacity
 		@planes = []
+		@weather = weather
 	end
 
 	def plane_count
 		planes.count
 	end	
 
-	def accept(plane,weather)
-		raise "Unable to land due to bad weather....Good luck!" if !sunny?
+	def accept(plane)
+		raise "Unable to land due to bad weather....Good luck!" if !@weather.sunny?
 		raise "Find another airport, we are full" if full?
 		plane.land	
 		@planes << plane
 	end
 	
-	def release(plane,weather)
+	def release(plane)
 		raise "This plane has already taken off" if !@planes.include?(plane)
 		plane.take_off
 		@planes.delete(plane)
-
 	end
 
 	def full?
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,7 +6,7 @@ describe Airport do
 	
 	let(:plane) { double :plane }
 	let(:weather) { double :weather }
-	let(:airport) { Airport.new }
+	let(:airport) { Airport.new(weather) }
 
 		it 'should initialize with the default capacity and no planes' do
 			expect(airport.capacity).to eq(15)
@@ -17,42 +17,42 @@ describe Airport do
 
 		before do
 			# only applied in this context
-			airport.stub(:sunny?).and_return(true)
+			weather.stub(:sunny?).and_return(true)
 		end
 
 		it "should be able to recieve a plane" do
 			expect(plane).to receive(:land)
-			airport.accept(plane,weather)
+			airport.accept(plane)
 			expect(airport.plane_count).to eq(1)
 		end
 
 		it "should be able to release a plane" do
 			expect(plane).to receive(:land)
-			airport.accept(plane,weather)
+			airport.accept(plane)
 			expect(plane).to receive(:take_off)
-			airport.release(plane,weather)
+			airport.release(plane)
 			expect(airport.plane_count).to eq(0)
 		end
 
 		it 'should not allow a plane to land if full' do 
-			(airport.capacity).times { airport.accept(Plane.new, weather) }
+			(airport.capacity).times { airport.accept(Plane.new) }
 			expect(airport).to be_full
-			expect{ (airport.accept(plane, weather)) }.to raise_error(RuntimeError)
+			expect{ (airport.accept(plane)) }.to raise_error(RuntimeError)
 		end
 
 		it 'should know if a plane has already taken off' do
 			expect(plane).to receive(:land)
-			airport.accept(plane, weather)
+			airport.accept(plane)
 			expect(plane).to receive(:take_off)
-			airport.release(plane, weather)
-			expect{ (airport.release(plane, weather)) }.to raise_error(RuntimeError)
+			airport.release(plane)
+			expect{ (airport.release(plane)) }.to raise_error(RuntimeError)
 		end
 	end
 
 	context "in bad weather" do 
 
 		before(:each) do
-			airport.stub(:sunny?).and_return(false)
+			weather.stub(:sunny?).and_return(false)
 		end
 
 		it "should not be able to land" do
@@ -60,18 +60,12 @@ describe Airport do
 			expect(airport.plane_count).to eq(0)
 		end
 
-
-		it 'should know if "stormy" or "sunny"' do
-			expect(airport.sunny?).to be_false
-		end
-
 		it 'should not allow plane to land when stormy' do
-			expect{ (airport.accept(plane, weather)) }.to raise_error(RuntimeError)
+			expect{ (airport.accept(plane)) }.to raise_error(RuntimeError)
 		end
 
 		it 'should not allow planes to take off when stormy' do
-			expect{ (airport.release(plane, weather)) }.to raise_error(RuntimeError)
+			expect{ (airport.release(plane)) }.to raise_error(RuntimeError)
 		end	
-
 	end
 end
